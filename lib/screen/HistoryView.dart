7cd7de483a515ac8a6033416c2a5e247d5ff3c0f
sooin923 <sooin051215@hanyang.ac.kr>
2025-06-23 21:39:01 +0900

    히스토리뷰 페이지 하단바 추가

--- lib/screen/HistoryView.dart
+++ lib/screen/HistoryView.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:idea_soop/const/Colors.dart';
 import 'package:idea_soop/screen/HistoryViewSearch.dart';
+import 'package:idea_soop/screen/home_screen.dart';
 
 class HistoryView extends StatefulWidget {
   @override
@@ -175,6 +176,7 @@ class _HistoryViewState extends State<HistoryView> {
         ),
       ),
       // 하단 네비게이션 등 추가 가능
+      bottomNavigationBar: HomeBottomNav(currentIndex: 1,),
     );
   }
 }
